d22df2b01da2fde44deb50f8d3cf501002f7a9d2
Alexandr <KBjergmineterra@gmail.com>
2023-02-28 14:46:02 +0200

    Interface remake

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         val text = editText.text.toString()
         val radius = text.toDouble()
         val pi = 3.14159
-        var area = round(pi * radius * radius)/10.0
+        var area = round(pi * radius * radius)
         var areaCircle:String = area.toString()
         textAreaCircle?.setText(areaCircle)
         textAreaCircle = findViewById(R.id.textAreaCircle)
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         val editText2 = findViewById<EditText>(R.id.edit_text_number_rectangle2)
         val text2 = editText2.text.toString()
         val sideRectangle2 = text2.toDouble()
-        val area =  round(sideRectangle * sideRectangle2)/10.0
+        val area =  round(sideRectangle * sideRectangle2)
         val areaRectangle:String = area.toString()
         textAreaRectangle?.setText(areaRectangle)
         textAreaRectangle = findViewById(R.id.textAreaRectangle)
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.edit_text_number_square)
         val text = editText.text.toString()
         val sideSquare = text.toDouble()
-        val area =  round(sideSquare * sideSquare)/10.0
+        val area =  round(sideSquare * sideSquare)
         val areaSquare:String = area.toString()
         textAreaSquare?.setText(areaSquare)
         textAreaSquare = findViewById(R.id.textAreaSquare)
