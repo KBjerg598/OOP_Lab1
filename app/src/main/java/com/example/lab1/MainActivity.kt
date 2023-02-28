e715f97749f2ab9fbb1e6e2cb8807defee576886
Alexandr <KBjergmineterra@gmail.com>
2023-02-28 14:23:15 +0200

    Square update

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -8,4 +8,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun calculateSquareArea(view: View) {
+        val editText = findViewById<EditText>(R.id.edit_text_number_square)
+        val text = editText.text.toString()
+        val sideSquare = text.toDouble()
+        val area =  round(sideSquare * sideSquare)/10.0
+        val areaSquare:String = area.toString()
+        textAreaSquare?.setText(areaSquare)
+        textAreaSquare = findViewById(R.id.textAreaSquare)
+    }
 }
